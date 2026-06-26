db3de1608b3ed10b10a5a01fc861114a3cf2f170
FranticToaster <201908132+FranticToaster@users.noreply.github.com>
2026-06-26 16:44:10 +0800

    Update yahtzee_game.py
    minor bugfixes

--- yahtzee_game.py
+++ yahtzee_game.py
@@ -45,7 +45,7 @@ def moveFitsReq(
         )
     
     elif category == LARGE_STRAIGHT:
-        return (dices == [1,1,1,1,1,0] or dices == [0,1,1,1,1,1])
+        return (dices == (1,1,1,1,1,0) or dices == (0,1,1,1,1,1))
     
     elif category == THREE_OF_A_KIND:
         return any(dices[i] >= 3 for i in range(6))
@@ -105,7 +105,7 @@ def claimCategory(
 
     moveScore = getMoveScore(game, category, (game >> 12) & 0b11111111)
     if category <= SIXES and upperSectionScore < 63:
-        upperSectionScore = max(
+        upperSectionScore = min(
             63,
             upperSectionScore + moveScore
         )
